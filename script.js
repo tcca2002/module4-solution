f7933c427335620e3b8a69147b599cd54ebd2781
Jaden Tran <tcca2002@yahoo.com>
2025-05-20 10:42:10 +1000

    Update script.js

--- script.js
+++ script.js
@@ -33,17 +33,15 @@ WARNING!!! WARNING!!!
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
-
+(function () {
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 // STEP 10:
 // Loop over the names array and say either 'Hello' or "Good Bye"
 // using either the helloSpeaker's or byeSpeaker's 'speak' method.
 // See Lecture 50, part 1
-for (var name in names) {
-  console.log("Hello " + names[name]);
 
-for (/* fill in parts of the 'for' loop to loop over names array */ i=0, i < names.length, i++) {
+for (/* fill in parts of the 'for' loop to loop over names array */ i=0; i < names.length; i++) {
 
   // STEP 11:
   // Retrieve the first letter of the current name in the loop.
@@ -65,3 +63,5 @@ for (/* fill in parts of the 'for' loop to loop over names array */ i=0, i < nam
      helloSpeaker.speak(name[i]);
   }
 }
+
+})();
